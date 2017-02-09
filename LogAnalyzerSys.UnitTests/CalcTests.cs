945d8bd13581b967ad518923d23abbaaf7070c8d
yongyang.cai <yongyang.cai@YONGYANGCAI-PC>
2017-02-09 23:28:47 +0800

    go

--- LogAnalyzerSys.UnitTests/CalcTests.cs
+++ LogAnalyzerSys.UnitTests/CalcTests.cs
@@ -4,7 +4,7 @@ using NSubstitute;
 namespace LogAnalyzerSys.UnitTests
 {
     [TestClass]
-    public class UnitTest2
+    public class CalcTests
     {
         [TestMethod]
         public void Add_TheNumber2AndTheNumber3_ReturnCorrectResult()
@@ -12,6 +12,15 @@ namespace LogAnalyzerSys.UnitTests
             ICalculator instance = Substitute.For<ICalculator>();
             instance.Add(2, 3).Returns(5);
             Assert.AreEqual(5, instance.Add(2, 3));
+        }
+
+        [TestMethod]
+        public void Add_TwoNumber_CorrectAddition()
+        {
+            var instance = Substitute.For<ICalculator>();
+            instance.Add(Arg.Any<double>(), Arg.Any<double>()).Returns(x => (double)x[0] + (double)x[1]);
+            Assert.AreEqual(10, instance.Add(5, 5));
+
         }
     }
 }
