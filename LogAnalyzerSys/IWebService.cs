fbaaa07fdd5a7e863f20c3a74ff42333276c2be6
yongyang.cai <yongyang.cai@YONGYANGCAI-PC>
2017-02-09 19:59:56 +0800

    g

--- LogAnalyzerSys/IWebService.cs
+++ LogAnalyzerSys/IWebService.cs
@@ -5,7 +5,7 @@
         string LastError { get; set; }
 
         /// <summary>
-        /// 
+        /// 记录错误
         /// </summary>
         /// <param name="msg"></param>
         void LogError(string msg);
